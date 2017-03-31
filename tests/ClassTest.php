e58d0041b3109edca0b5070adbf0c1f28ae36396
Cyrus Eftos <cyrus.eftos@commerce.wa.gov.au>
2017-03-31 14:45:37 +0800

    Addition of new names and related nicknames.

--- tests/ClassTest.php
+++ tests/ClassTest.php
@@ -7,17 +7,33 @@ use CommerceWA\Nicknames\Nicknames;
 
 class ClassTest extends PHPUnit_Framework_TestCase
 {
+
     /**
      * Verify basic behavior of getName().
      *
      * @test
      * @covers ::__construct
-     * @covers ::givenTo
      * @covers ::getName
      * @uses \CommerceWA\Nicknames\Nicknames
      *
      * @return void
      */
+    public function testJSONFile()
+    {
+        $json = json_decode(file_get_contents(realpath(__DIR__ . '/../src/names.json')), true);
+        $this->assertTrue(json_last_error() === 0);
+    }
+    /**
+     * Verify basic behavior of getName().
+     *
+     * @test
+     * @covers ::__construct
+     * @covers ::getName
+     * @uses \CommerceWA\Nicknames\Nicknames
+     *
+     * @return void
+     */
+
     public function testGetName()
     {
         $nicknames = new Nicknames();
